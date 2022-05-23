3134ec9121ce2a6b37f00e75b3b34e5fc41398a2
João Otávio da Silva <joao.otavio1225@gmail.com>
2022-05-23 11:06:03 -0300

    Adicionando o Swagger na API

---
+++ 11 RestWithSpringBootUdemy Swagger/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `las_name`) VALUES
+	(1, 'Uberlandia - Minas Gerais - Brasil', 'Leandro', 'Male', 'Costa'),
+	(2, 'Uberlandia - Minas Gerais - Brasil', 'Gabriela', 'Female', 'Costa'),
+	(5, 'Patos de Minas - Minas Gerais - Brasil', 'Flávio', 'Male', 'Costa'),
+	(7, 'Uberlandia - Minas Gerais - Brasil', 'Fernanda', 'Female', 'Cardoso da Silva'),
+	(8, 'Patos de Minas - Minas Gerais - Brasil', 'Pedro', 'Male', 'Paulo'),
+	(9, 'Patos de Minas - Minas Gerais - Brasil', 'Marcos', 'Male', 'Paulo');
